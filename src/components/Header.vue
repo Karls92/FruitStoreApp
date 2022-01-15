fb13a921827a41cf14b8fe48efce4fa558a527e8
Karls92 <nit_1905@hotmail.com>
2022-01-15 16:17:55 -0400

    The Header component has been created.

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <img alt="Store logo" class="logo-img" src="../assets/logo/shops-512.png">
+                <h1>Fruit Market</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h1{
+font-size: 40px;
+text-shadow: 0 0 5px #fff, 0 0 10px #000, 0 0 15px #000, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18, 2px 2px 2px rgba(82,227,32,0);
+}
+
+/* mobile */
+img.logo-img {
+width: 100%;
+}
+
+@media (min-width: 780px)
+{
+  img.logo-img {
+  width: 40%;
+  }
+}
+
+</style>
