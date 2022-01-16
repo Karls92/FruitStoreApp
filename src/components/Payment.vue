9c8d82f232e5cf23367c5f15c5f1c849038a81cd
Karls92 <nit_1905@hotmail.com>
2022-01-16 00:02:50 -0400

    a payment form has added to template

---
+++ src/components/Payment.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="sm-12">
+                <h2>Payment</h2>
+                <div class="mb-3 row">
+                    <label for="payment" class="col-sm-2 col-form-label">Payment</label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="payment">
+                    </div>
+                    <button class="btn btn-success">Pay</button>
+                </div>
+                <div class="mb-3 row">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Payment'
+}
+</script>
+
+<style scoped>
+</style>
