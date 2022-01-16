cf2105628c42e0d9b0e3ab3e641d2509effb30ab
Karls92 <nit_1905@hotmail.com>
2022-01-16 14:27:48 -0400

    Payment input is connected with the totalToPay and totalPaid value

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -5,7 +5,7 @@
                 <h2>Payment</h2>
                 <div class="row mb-2">
                     <div class="col-sm-12 col-md-8">
-                        <input type="number" step="0.01" :min="totalToPay" :value="totalToPay" class="form-control" id="payment_quant" placeholder="Money Received">
+                        <input type="number" step="0.01" :min="totalToPay" v-model="totalPaid" class="form-control" id="payment_quant" placeholder="Money Received">
                     </div>
                     <div class="col-sm-12 col-md-4 d-grid">
                         <button class="btn btn-success">Send Payment</button>
@@ -27,7 +27,7 @@ import { mapState } from 'vuex'
 export default {
 name: 'Payment',
 computed:{
-    ...mapState(['totalToPay'])
+    ...mapState(['totalToPay', 'totalPaid'])
  }
 }
 </script>
