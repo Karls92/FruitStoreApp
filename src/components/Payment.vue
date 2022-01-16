e8432095670c66c327751b0117af3bb51d55b65d
Karls92 <nit_1905@hotmail.com>
2022-01-16 00:21:19 -0400

    Payment form added

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -3,12 +3,14 @@
         <div class="row">
             <div class="sm-12">
                 <h2>Payment</h2>
-                <div class="mb-3 row">
-                    <label for="payment" class="col-sm-2 col-form-label">Payment</label>
-                    <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="payment">
+                <div class="row mb-3">
+                    <div class="col-auto">
+                        <label for="payment_quant" class="visually-hidden">Money Received</label>
+                        <input type="number" step="0.01" min="0.00" class="form-control" id="payment_quant" placeholder="Money Received">
                     </div>
-                    <button class="btn btn-success">Pay</button>
+                    <div class="col-auto">
+                        <button class="btn btn-success mb-3">Send Payment</button>
+                    </div>
                 </div>
                 <div class="mb-3 row">
                     
