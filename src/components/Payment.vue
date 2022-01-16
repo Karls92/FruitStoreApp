11afd92fd8c33b880f9530b709ef61f8c41f7824
Karls92 <nit_1905@hotmail.com>
2022-01-16 00:40:36 -0400

    Margin and Padding changes added to the payment form

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="container">
+    <div class="container mt-3 mb-3">
         <div class="row">
-            <div class="sm-12">
+            <div class="col-sm-12">
                 <h2>Payment</h2>
-                <div class="row mb-3">
-                    <div class="col-auto">
-                        <label for="payment_quant" class="visually-hidden">Money Received</label>
+                <div class="row mb-2">
+                    <div class="col-sm-12 col-md-8">
                         <input type="number" step="0.01" min="0.00" class="form-control" id="payment_quant" placeholder="Money Received">
                     </div>
-                    <div class="col-auto">
-                        <button class="btn btn-success mb-3">Send Payment</button>
+                    <div class="col-sm-12 col-md-4 d-grid">
+                        <button class="btn btn-success">Send Payment</button>
                     </div>
                 </div>
-                <div class="mb-3 row">
+                <div class="row">
                     
                 </div>
             </div>
