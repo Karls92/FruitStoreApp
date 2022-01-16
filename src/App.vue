b87c3cf082e6437e6f71348bd738f5b24a9f5d2a
Karls92 <nit_1905@hotmail.com>
2022-01-15 22:16:52 -0400

    Add new component called StockList.vue in the component list and in the template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header></Header>
+    <StockList></StockList>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import StockList from './components/StockList'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    StockList
   }
 }
 </script>
