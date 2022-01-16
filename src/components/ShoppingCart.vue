0674695b362d407cbe39cc9e43716246f9d9d517
Karls92 <nit_1905@hotmail.com>
2022-01-15 23:35:20 -0400

    Table added to show the products that user selects from the stock list. Also the total to payment is shon in the template

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+       <div class="row">
+           <div class="sm-12">
+                <h2>Shopping Cart</h2>
+                 
+                 <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="product of shoppingCart" :key="product.id">
+                        <td>{{fruits[product.fruitId].name}}</td> 
+                        <td>{{product.quant}} Kg</td>
+                        <td>{{product.subtotal.toFixed(2)}} $</td>
+                        </tr>
+                    </tbody>
+                </table>
+           </div>
+           <div class="sm-12">
+                <div class="alert alert-info" role="alert">
+                    <h3>TOTAL TO PAY: {{totalToPay.toFixed(2)}} $ </h3> 
+                </div>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+name: 'ShoppingCart',
+computed: {
+    ...mapState(['fruits', 'shoppingCart', 'totalToPay'])
+ }
+}
+</script>
+
+<style scoped>
+</style>
