5eabedb583447acf3aed159086dbdcb9db8028fa
Karls92 <nit_1905@hotmail.com>
2022-01-16 16:15:52 -0400

    Delete the remaining money section finish the component changes

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -4,19 +4,14 @@
             <div class="col-sm-12">
                 <h2>Payment</h2>
                 <div class="row mb-2">
-                    <div class="col-sm-12 col-md-8">
-                        <input type="number" step="0.01" :min="totalToPay" v-model="totalPaid" class="form-control" id="payment_quant" placeholder="Money Received">
+                    <div class="col-sm-12 col-md-7 d-grid">
+                        <input type="text" :value="totalToPay.toFixed(2)" class="form-control" id="payment_quant" placeholder="Money Received" disabled>
                     </div>
-                    <div class="col-sm-12 col-md-4 d-grid">
+                    <div class="col-sm-12 col-md-5 d-grid">
                         <button class="btn btn-success" @click="proccessPayment">Send Payment</button>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h3>Your remaining money</h3>
-                        <h4>0.00 $</h4>
-                    </div>
-                </div>
+        
             </div>
         </div>
     </div>
@@ -26,8 +21,10 @@
 import { mapState, mapMutations } from 'vuex'
 export default {
 name: 'Payment',
-computed:{
-    ...mapState(['totalToPay', 'totalPaid']),
+computed: {
+    ...mapState(['totalToPay'])
+ },
+ methods: {
     ...mapMutations(['proccessPayment']) 
  }
 }
